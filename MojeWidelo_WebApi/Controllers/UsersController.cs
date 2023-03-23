c70015a127dc09e57e02b5cfa392e02e391b9125
sosna <jakub.sosnowski2001@gmail.com>
2023-03-23 04:05:25 +0100

    Add /api/login endpoint and authorization logic

--- MojeWidelo_WebApi/Controllers/UsersController.cs
+++ MojeWidelo_WebApi/Controllers/UsersController.cs
@@ -2,12 +2,19 @@
 using Contracts;
 using Entities.Data;
 using Entities.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using MojeWidelo_WebApi.Filters;
+using MojeWidelo_WebApi.Models;
+using System.IdentityModel.Tokens.Jwt;
 using System.Net.Mime;
+using System.Security.Claims;
+using System.Text;
 
 namespace MojeWidelo_WebApi.Controllers
 {
+    [Route("api/")]
     [ApiController]
     public class UsersController : ControllerBase
     {
@@ -28,6 +35,7 @@ namespace MojeWidelo_WebApi.Controllers
         /// <response code="400">Bad request</response>
         /// <response code="401">Unauthorized</response>
         [HttpGet("getAll", Name = "GetAll")]
+        [Authorize]
         [Produces(MediaTypeNames.Application.Json, Type = typeof(IEnumerable<UserDTO>))]
         public async Task<IActionResult> GetAllUsers()
         {
@@ -46,6 +54,7 @@ namespace MojeWidelo_WebApi.Controllers
         /// <response code="401">Unauthorized</response>
         /// <response code="404">Not found</response>
         [HttpGet("user/{id}", Name = "getUserById")]
+        [Authorize]
         [ServiceFilter(typeof(ObjectIdValidationFilter))]
         [Produces(MediaTypeNames.Application.Json, Type = typeof(UserDTO))]
         public async Task<IActionResult> GetUserById(string id)
@@ -64,6 +73,7 @@ namespace MojeWidelo_WebApi.Controllers
         /// <response code="400">Bad request</response>
         /// <response code="401">Unauthorized</response>
         [HttpPut("user", Name = "updateUser")]
+        [Authorize]
         [ServiceFilter(typeof(ModelValidationFilter))]
         [Produces(MediaTypeNames.Application.Json, Type = typeof(UserDTO))]
         public async Task<IActionResult> UpdateUser([FromBody] UpdateUserDTO userDTO)
@@ -98,11 +108,40 @@ namespace MojeWidelo_WebApi.Controllers
         /// <response code="400">Bad request</response>
         /// <response code="401">Unauthorized</response>
         [HttpDelete("user/{id}", Name = "deleteUser")]
+        [Authorize]
         [ServiceFilter(typeof(ObjectIdValidationFilter))]
         public async Task<IActionResult> DeleteUser(string id)
         {
             await _repository.UsersRepository.Delete(id);
             return Ok();
+        }
+
+        [HttpPost, Route("login")]
+        public IActionResult Login([FromBody] LoginModel user)
+        {
+            if (user == null)
+            {
+                return BadRequest("Invalid client request!");
+            }
+
+            if (user.Username == "NIE@MOGE" && user.Password == "ODDYCHAC")
+            {
+                var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("hasloooo1234$#@!"));
+                var signingCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+                var tokenOptions = new JwtSecurityToken(
+                    issuer: "https://localhost:5001",
+                    audience: "https://localhost:5001",
+                    claims: new List<Claim>(),
+                    expires: DateTime.Now.AddMinutes(5),
+                    signingCredentials: signingCredentials
+                );
+
+                var tokenString = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+                return Ok(new { Token = tokenString });
+            }
+
+            return Unauthorized();
         }
     }
 }
--- MojeWidelo_WebApi/Program.cs
+++ MojeWidelo_WebApi/Program.cs
@@ -1,4 +1,8 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using MojeWidelo_WebApi.Extensions;
+using Microsoft.IdentityModel.Tokens;
+using Amazon.SecurityToken.Model.Internal.MarshallTransformations;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,11 +17,38 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 #endregion
 
+void ConfigureServices(IServiceCollection services)
+{
+    services.AddAuthentication(opt =>
+    {
+        opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+
+            ValidIssuer = "https://localhost:5001",
+            ValidAudience = "https://localhost:5001",
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("hasloooo1234$#@!"))
+        };
+
+    });
+
+    services.AddControllers();
+}
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.ConfigureSwagger();
+ConfigureServices(builder.Services);
 
 var app = builder.Build();
 
@@ -28,7 +59,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
