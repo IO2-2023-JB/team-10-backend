f82816d27f89752eb28de8261524d2bf5ebcf13c
nowakmikolaj <nowak.mikolaj19@gmail.com>
2023-03-29 23:44:33 +0200

    Rebase video-metadata onto register-endpoint

---
+++ MojeWidelo_WebApi/Controllers/BaseController.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MojeWidelo_WebApi.Controllers
+{
+    public class BaseController : ControllerBase
+    {
+        protected readonly IRepositoryWrapper _repository;
+        protected readonly IMapper _mapper;
+
+        public BaseController(IRepositoryWrapper repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+    }
+}
--- Repository/RepositoryWrapper.cs
+++ Repository/RepositoryWrapper.cs
@@ -1,14 +1,14 @@
-﻿using Contracts;
-
-namespace Repository
+﻿namespace Repository
 {
-	public class RepositoryWrapper : IRepositoryWrapper
-	{
-		public IUsersRepository UsersRepository { get; }
+    public class RepositoryWrapper : IRepositoryWrapper
+    {
+        public IUsersRepository UsersRepository { get; }
+        public IVideoRepository VideoRepository { get; }
 
-		public RepositoryWrapper(IUsersRepository usersRepository)
-		{
-			UsersRepository = usersRepository;
-		}
-	}
+        public RepositoryWrapper(IUsersRepository usersRepository, IVideoRepository videoRepository)
+        {
+            UsersRepository = usersRepository;
+            VideoRepository = videoRepository;
+        }
+    }
 }
