8bbcdd3f5c710d211929da2f0253495870d63f8a
sosna <jakub.sosnowski2001@gmail.com>
2023-03-30 15:05:43 +0200

    Add comparing hashed passwords + add fixes from review

--- Entities/Data/LoginDTO.cs
+++ Entities/Data/LoginDTO.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Entities.Data
 {
@@ -11,7 +7,6 @@ namespace Entities.Data
         [EmailAddress]
         public string Email { get; set; }
         public string Password { get; set; }
-
     }
 
     public class LoginResponseDto
