4d2029184950174b1794c88b8747d5c42f1c5461
nowakmikolaj <102852926+nowakmikolaj@users.noreply.github.com>
2023-04-04 12:53:17 +0200

    Add mock configuration and example tests (#11)

---
+++ MojeWidelo_WebApi.UnitTests/Mocks/MockIRepositoryWrapper.cs
@@ -0,0 +1,19 @@
+﻿using Contracts;
+using Moq;
+
+namespace MojeWidelo_WebApi.UnitTests.Mocks
+{
+	public class MockIRepositoryWrapper
+	{
+		public static Mock<IRepositoryWrapper> GetMock()
+		{
+			var mock = new Mock<IRepositoryWrapper>();
+
+			var usersRepoMock = MockIUsersRepository.GetMock();
+
+			mock.Setup(m => m.UsersRepository).Returns(() => usersRepoMock.Object);
+
+			return mock;
+		}
+	}
+}
