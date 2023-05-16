49527dd4ae923444a9497203b47cebaed78092bb
sosna <jakub.sosnowski2001@gmail.com>
2023-05-17 00:34:08 +0200

    Add fixes from review 2

--- MojeWidelo_WebApi/Controllers/DonateController.cs
+++ MojeWidelo_WebApi/Controllers/DonateController.cs
@@ -5,62 +5,73 @@ using Microsoft.AspNetCore.Mvc;
 using MojeWidelo_WebApi.Filters;
 using System.ComponentModel.DataAnnotations;
 
-
 namespace MojeWidelo_WebApi.Controllers
 {
-    [ApiController]
-    public class DonateController : BaseController
-    {
-        public DonateController(IRepositoryWrapper repository, IMapper mapper)
-            : base(repository, mapper)
-        {
-        }
+	[ApiController]
+	public class DonateController : BaseController
+	{
+		public DonateController(IRepositoryWrapper repository, IMapper mapper)
+			: base(repository, mapper) { }
 
-        [HttpPost("/donate/send", Name = "sendDonation")]
-        [ServiceFilter(typeof(ObjectIdValidationFilter))]
-        public async Task<IActionResult> SendDonation([Required] string id, [Required] decimal amount)
-        {
-            var user = await _repository.UsersRepository.GetById(id);
+		[HttpPost("/donate/send", Name = "sendDonation")]
+		[ServiceFilter(typeof(ObjectIdValidationFilter))]
+		public async Task<IActionResult> SendDonation([Required] string id, [Required] decimal amount)
+		{
+			if (id == GetUserIdFromToken())
+			{
+				return StatusCode(StatusCodes.Status400BadRequest, "Nie można wysłać dotacji samemu sobie.");
+			}
 
-            if (user == null)
-            {
-                return StatusCode(StatusCodes.Status404NotFound, "Użytkownik o podanym ID nie istnieje.");
-            }
+			var user = await _repository.UsersRepository.GetById(id);
 
-            if (user.UserType != UserType.Creator)
-            {
-                return StatusCode(StatusCodes.Status400BadRequest, "Użytkownik o podanym ID nie jest twórcą.");
-            }
+			if (user == null)
+			{
+				return StatusCode(StatusCodes.Status404NotFound, "Użytkownik o podanym ID nie istnieje.");
+			}
 
-            await _repository.UsersRepository.UpdateAccountBalance(id, amount);
+			if (user.UserType != UserType.Creator)
+			{
+				return StatusCode(StatusCodes.Status400BadRequest, "Użytkownik o podanym ID nie jest twórcą.");
+			}
 
-            return StatusCode(StatusCodes.Status200OK, "Dotacja została wysłana pomyślnie.");
-        }
+			if (amount <= 0)
+			{
+				return StatusCode(StatusCodes.Status400BadRequest, "Podana wartość musi być większa od zera.");
+			}
 
-        [HttpPost("/donate/withdraw", Name = "withdrawFunds")]
-        public async Task<IActionResult> WithdrawFunds([Required] decimal amount)
-        {
-            var id = GetUserIdFromToken();
+			await _repository.UsersRepository.UpdateAccountBalance(id, amount);
 
-            var user = await _repository.UsersRepository.GetById(id);
+			return StatusCode(StatusCodes.Status200OK, "Dotacja została wysłana pomyślnie.");
+		}
 
-            if (user.UserType != UserType.Creator)
-            {
-                return StatusCode(StatusCodes.Status403Forbidden, "Nie można wypłacić środków, nie jesteś twórcą.");
-            }
+		[HttpPost("/donate/withdraw", Name = "withdrawFunds")]
+		public async Task<IActionResult> WithdrawFunds([Required] decimal amount)
+		{
+			var id = GetUserIdFromToken();
 
-            if (user.AccountBalance < amount)
-            {
-                return StatusCode(
-                    StatusCodes.Status400BadRequest,
-                    "Nie można wypłacić środków, niewystarczająca ilość środków na koncie."
-                );
-            }
+			var user = await _repository.UsersRepository.GetById(id);
 
-            await _repository.UsersRepository.UpdateAccountBalance(id, -amount);
+			if (user.UserType != UserType.Creator)
+			{
+				return StatusCode(StatusCodes.Status403Forbidden, "Nie można wypłacić środków, nie jesteś twórcą.");
+			}
 
-            return StatusCode(StatusCodes.Status200OK, "Środki zostały wypłacone pomyślnie.");
-        }
+			if (amount <= 0)
+			{
+				return StatusCode(StatusCodes.Status400BadRequest, "Podana wartość musi być większa od zera.");
+			}
 
-    }
+			if (user.AccountBalance < amount)
+			{
+				return StatusCode(
+					StatusCodes.Status400BadRequest,
+					"Nie można wypłacić środków, niewystarczająca ilość środków na koncie."
+				);
+			}
+
+			await _repository.UsersRepository.UpdateAccountBalance(id, -amount);
+
+			return StatusCode(StatusCodes.Status200OK, "Środki zostały wypłacone pomyślnie.");
+		}
+	}
 }
