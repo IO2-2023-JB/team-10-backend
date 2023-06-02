b15f3e89284f63a7c00f4871c56407479a19082a
Mateusz Zagórski <mateusz.zagorski2@gmail.com>
2023-06-02 02:16:08 +0200

    MW-230 Add reactions respository and controller tests

--- MojeWidelo_WebApi.UnitTests/Mocks/MockIRepositoryWrapper.cs
+++ MojeWidelo_WebApi.UnitTests/Mocks/MockIRepositoryWrapper.cs
@@ -13,11 +13,13 @@ namespace MojeWidelo_WebApi.UnitTests.Mocks
 			var subscriptionsRepoMock = MockISubscriptionsRepository.GetMock();
 			var commentsRepoMock = MockICommentsRepository.GetMock();
 			var videosRepoMock = MockIVideosRepository.GetMock();
+			var reactionsRepoMock = MockIReactionRepository.GetMock();
 
 			mock.Setup(m => m.UsersRepository).Returns(() => usersRepoMock.Object);
 			mock.Setup(m => m.SubscriptionsRepository).Returns(() => subscriptionsRepoMock.Object);
 			mock.Setup(m => m.CommentRepository).Returns(() => commentsRepoMock.Object);
 			mock.Setup(m => m.VideoRepository).Returns(() => videosRepoMock.Object);
+			mock.Setup(m => m.ReactionRepository).Returns(() => reactionsRepoMock.Object);
 
 			return mock;
 		}
