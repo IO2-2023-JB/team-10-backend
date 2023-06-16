c2aa409d5c49270b403a68664b1101d3388a40c5
H3w9VH3F4L4Tq5MbsfRi <rizezir@gmail.com>
2023-06-16 07:51:54 +0200

    Logic fix as per Mikołaj's comment

--- MojeWidelo_WebApi/Controllers/UsersController.cs
+++ MojeWidelo_WebApi/Controllers/UsersController.cs
@@ -11,6 +11,7 @@ using MojeWidelo_WebApi.Helpers;
 using Repository.Managers;
 using System.ComponentModel.DataAnnotations;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Net.Mime;
 using System.Security.Claims;
 using System.Text;
@@ -173,7 +174,9 @@ namespace MojeWidelo_WebApi.Controllers
 			}
 			await _repository.UsersRepository.Delete(id);
 
-			var subs = await _repository.SubscriptionsRepository.GetUserSubscriptions(senderId);
+			var subs = await _repository.SubscriptionsRepository.GetUserSubscriptions(id);
+			subs = subs.Concat(await _repository.SubscriptionsRepository.GetCreatorSubscriptions(id));
+
 			foreach (var sub in subs)
 			{
 				await _repository.SubscriptionsRepository.Delete(sub.Id);
